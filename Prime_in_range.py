4b0ff016ffe85138b9edc6e4d15c64ada7802af9
MK1Github <32424908+MK1Github@users.noreply.github.com>
2017-10-10 21:43:58 -0700

    v2
    v2

--- Prime_in_range.py
+++ Prime_in_range.py
@@ -1,9 +1,10 @@
 #min=int(input("100:"))
 #max=int(input("200:"))
+import math
 min=100
 max=200
 for num in range(min,max):
-    for x in range(2,num):
+    for x in range(2,int(math.sqrt(num))+1):
         if(num%x==0 and num!=1):
             break
         else:
