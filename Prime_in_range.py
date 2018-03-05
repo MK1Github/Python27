e63a2fdc9ae1c553c5e353c73c68a69619df757e
MK1Github <32424908+MK1Github@users.noreply.github.com>
2018-03-04 19:30:02 -0800

    Revert "v2"
    This reverts commit 4b0ff016ffe85138b9edc6e4d15c64ada7802af9.

--- Prime_in_range.py
+++ Prime_in_range.py
@@ -1,10 +1,9 @@
 #min=int(input("100:"))
 #max=int(input("200:"))
-import math
 min=100
 max=200
 for num in range(min,max):
-    for x in range(2,int(math.sqrt(num))+1):
+    for x in range(2,num):
         if(num%x==0 and num!=1):
             break
         else:
